f1785fcc2238c5fab2c9203856080dbc29a4fa5d
Chard Kevin H. Estinopo <243772172+CKevinPro@users.noreply.github.com>
2025-11-14 07:42:31 +0800

    Add files via upload

---
+++ Activity1.py
@@ -0,0 +1,12 @@
+# This is a comment
+# This is my first Python Program
+# I am Chard Kevin H. Estinopo, of BSIT - 1A
+
+print("Hello World!")
+
+
+
+
+
+
+
---
+++ Activity2.py
@@ -0,0 +1,19 @@
+# CC101
+# Estinopo, Chard Kevin H.
+# BSIT - 1A
+
+# My Favorite Online Game or Movie
+
+title = "Dr. Stone"
+main_character = "Senku Ishigami"
+seasons = "1 to 4"
+setting = "a world where humanity was turned into stone for thousands of years"
+favorite_scene = "when Senku creates electricity and the lightbulb"
+reason = "because the show blends science, survival, and friendship in a fun and inspiring way"
+
+print("My favorite anime is:", title + ".")
+print("The main character is", main_character + ", a genius who loves science.")
+print("The story takes place in", setting + ".")
+print("Across Seasons", seasons + ", Senku works to rebuild civilization using scientific knowledge.")
+print("One of my favorite scenes is", favorite_scene + ".")
+print("I enjoy watching it", reason + ".")
